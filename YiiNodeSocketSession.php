876a5a4373e014ecb34ab12c624a5fcec2c07a6a
Digit <digit.vova@gmail.com>
2017-02-13 11:34:35 +0200

    Some fixes in YiiNodeSocketSession component

--- YiiNodeSocketSession.php
+++ YiiNodeSocketSession.php
@@ -24,13 +24,26 @@ class YiiNodeSocketSession extends Session {
     public function open()
     {
         parent::open();
-        if(Yii::$app->nodeSockets && !empty(Yii::$app->nodeSockets->channelsByPermissions)) {
+        $components = Yii::$app->components;
+        if(isset($components['nodeSockets']) && Yii::$app->nodeSockets && !empty(Yii::$app->nodeSockets->channelsByPermissions)) {
             /* @var $nodeSockets YiiNodeSocket */
             $nodeSockets = Yii::$app->nodeSockets;
             foreach (Yii::$app->nodeSockets->channelsByPermissions as $channel => $permission) {
                 $can = $permission == '*' || (!Yii::$app->user->isGuest && Yii::$app->user->can($permission));
                 if($can) $nodeSockets->addUserSessionChannel($channel);
             }
+        }
+    }
+
+    /**
+     * Updates the current session ID with a newly generated one .
+     * Please refer to <http://php.net/session_regenerate_id> for more details.
+     * @param boolean $deleteOldSession Whether to delete the old associated session file or not.
+     */
+    public function regenerateID($deleteOldSession = false)
+    {
+        if ($this->getIsActive()) {
+            @session_regenerate_id($deleteOldSession);
         }
     }
 }
